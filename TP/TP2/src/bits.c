77e76bfd70dc98d9fd5d7d11f8ead57a66446ed6
Rémi <remi.gruffat@cpe.fr>
2020-11-02 17:43:05 +0100

    Update

---
+++ TP/TP2/src/bits.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+  int n, c, k;
+int tableau[31];
+  printf("Entrez votre nombre décimal\n");
+  scanf("%d", &n);
+
+  printf("%d en binaire est:\n", n);
+
+  for (c = 31; c >= 0; c--)
+  {
+    k = n >> c;
+
+    if (k & 1) {
+      printf("1");
+      tableau[c]=1;
+}
+    else {
+      printf("0");
+      tableau[c]=0;
+	}
+  }
+
+  printf("\n");
+
+//printf("%d\n",tableau[28]);
+
+if (tableau[28] & tableau[12] == 1)
+
+printf("1\n");
+
+else 
+
+printf("0\n");
+
+
+  return 0;
+}
