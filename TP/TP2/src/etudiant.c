ec8af4affdec9bbcdf1dfa9d27b6a4bef6afe735
Rémi <remi.gruffat@cpe.fr>
2020-11-02 18:22:07 +0100

    Updates

---
+++ TP/TP2/src/etudiant.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+char nom[20]="Gruffat";
+char Prenom[20]="Rémi";
+char address[50]="53 rue du vatican";
+char mod1[10]="19";
+char mod2[10]="13";
+
+printf("%s\n",nom);
+printf("%s\n",Prenom);
+printf("%s\n",address);
+printf("%s\n",mod1);
+printf("%s\n",mod2);
+
+
+
+
+
+
+return 0;
+}
