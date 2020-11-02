e6200642b9953805d45bd18147fdb0eecc78c651
Rémi <remi.gruffat@cpe.fr>
2020-11-02 15:11:17 +0100

    Changes

---
+++ TP/TP1/src/opérateurs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+
+float a = 16;
+float b = 3;
+int c = 16;
+int d = 3;
+printf("%f + %f = %f \n",a,b, a+b);
+printf("%f - %f = %f \n",a,b, a-b);
+printf("%f - %f = %f \n",b,a, b-a);
+printf("%f * %f = %f \n",a,b, a*b);
+printf("%f / %f = %f \n",a,b, a/b);
+printf("%f / %f = %f \n",b,a, b/a);
+printf("%d modulo  %d = %d \n",c,d, c % d);
+printf("%d modulo %d = %d \n",d,c, d % c);
+
+
+
+
+return 0;
+}
