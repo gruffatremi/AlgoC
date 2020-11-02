8b7e0df6e593702972a9ede4135b373511012cdd
Rémi <remi.gruffat@cpe.fr>
2020-11-02 17:06:10 +0100

    Update

---
+++ TP/TP2/src/fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+
+int U0 = 1;
+int U1 = 1;
+int Next; 
+int i;
+int n = 0;
+
+printf("Entrez combien d'occurence de la suite de fibonnaci que vous voulez ");
+scanf("%d",&n);
+
+printf("1\n1\n");
+
+
+
+for(i=0; i < n ; i++ ) {
+
+Next = U0+U1;
+
+printf("%d\n",Next);
+
+U0=U1;
+
+U1=Next; 
+
+}
+
+
+
+
+return 0; 
+}
